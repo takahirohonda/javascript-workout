ffc1b29be8b3b72e149275fce79eb7b9fd1e431a
mydatahack <thon6thon6@hotmail.com>
2021-04-23 23:59:57 +1000

    Update font support

--- playground/parcel-ts-react/src/Index.tsx
+++ playground/parcel-ts-react/src/Index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
 import './fonts/Lato/Lato.css';
+import App from './App';
+
 
 render(<App />, document.getElementById('app'));
